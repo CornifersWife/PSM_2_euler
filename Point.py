44f04ef5923b0b7fcd636d2e66e13e5274d4c6ee
CornifersWife <irys.latosz@gmail.com>
2023-03-21 19:53:48 +0100

    Basics done, need to add improved formula and better the axis

--- Point.py
+++ Point.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 class Point:
     def __init__(self, mass, x=0, y=0, vx=0, vy=0):
         self.mass = mass
@@ -6,13 +10,64 @@ class Point:
         self.vx = vx
         self.vy = vy
 
-    def apply_force(self, fx, fy, dt):
-        ax = fx / self.mass
-        ay = fy / self.mass
+    def apply_forces(self, fx, fy, dt, q):
+        ax = (fx - (q * self.vx)) / self.mass
+        ay = (fy - (q * self.vy)) / self.mass
         self.vx += ax * dt
         self.vy += ay * dt
         self.x += self.vx * dt
         self.y += self.vy * dt
 
-    def get_position(self):
-        return self.x, self.y
+    def update_position(self, dt):
+        self.x += self.vx * dt
+        self.y += self.vy * dt
+
+    def update_euler_improved(self, x, y, vx, vy, ax, ay, dt):
+        # calculate intermediate position and velocity values using Euler's method
+        x_intermediate = x + vx * dt / 2
+        y_intermediate = y + vy * dt / 2
+        vx_intermediate = vx + ax * dt / 2
+        vy_intermediate = vy + ay * dt / 2
+
+        # calculate the updated position and velocity values using the intermediate values
+        x = x + vx_intermediate * dt
+        y = y + vy_intermediate * dt
+        vx= vx + ax * dt
+        vy = vy + ay * dt
+    def simulate_motion(self, fx, fy, dt, q):
+        t = 0
+        positions = [(self.x, self.y)]
+        times = [0]
+        vxs = [self.vx]
+        vys = [self.vy]
+
+        while self.y >= 0:
+            # apply the given forces to the object
+            self.apply_forces(fx, fy, dt, q)
+
+            # update the object's position and velocity using Euler's improved method
+            self.update_position(dt)
+
+            # increment the time and append the object's position and time to the lists
+            t += dt
+            positions.append((self.x, self.y))
+            times.append(t)
+            vxs.append(self.vx)
+            vys.append(self.vy)
+
+        # plot the object's trajectory using linear interpolation
+        x_coords, y_coords = zip(*positions)
+        t_interp = np.linspace(0, t, len(x_coords))
+        print(f'{len(times)}    {len(x_coords)}')
+        interp_x_coords = np.interp(t_interp, times, x_coords)
+        interp_y_coords = np.interp(t_interp, times, y_coords)
+        fp = interp_y_coords  # only include y-coordinates in fp
+        xp = t_interp
+        plt.plot(interp_x_coords, interp_y_coords, color='red')
+        plt.xlabel('X Position')
+        plt.ylabel('Y Position')
+        plt.title('Projectile Trajectory')
+        plt.show()
+        # return the time taken for the object to hit the ground
+        return t
+
--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+from Point import Point
+from Force import Force
 
-if __name__ == '__main__':
-    print('PyCharm')
+# create a Point object with mass 1 and initial position (0, 0) and velocity (10, 10)
+point = Point(1, vx=10, vy=10)
+dt = 0.01
+q = 0.10
+forces = []
+f1 = Force(9.8, 270)
+#f2 = Force(1, 45)
 
+final_force = Force.total_force(f1)
+fx, fy = final_force.xy_forces()
+print(f'fx: {fx}     fy: {fy}')
+# simulate the movement for the given forces and time step and plot the trajectory
+t = point.simulate_motion(fx, fy, dt, q)
+
+# print the time taken for the object to hit the ground
+print(f"The object hit the ground after {t:.2f} seconds.")
