7bd6955c81f337efe4ec9ae6f653c110f2167e66
CornifersWife <irys.latosz@gmail.com>
2023-03-26 11:12:52 +0200

    done

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from Point import Point
 from Force import Force
 
 point = Point(1, vx=10, vy=10)
-dt = 0.01
-q = 0.05
+dt = 0.02
+q = 0.2
 forces = []
 forces.append(Force(10, 270))
-forces.append(Force(1, 45))
+#forces.append(Force(1, 45))
 final_force = Force.total_force(*forces)
 fx, fy = final_force.xy_forces()
 
